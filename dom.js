b34d4837b870caf907e75cd0760f8e87a01d83ed
Vinay <vinay8309963@gmail.com>
2023-08-22 17:32:19 +0530

    Item Lister Code Added

---
+++ dom.js
@@ -0,0 +1,6 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+header.style.border='solid 6px #000';
+var items = document.getElementsByClassName('title');
+items[0].style.fontWeight = 'bold';
+items[0].style.color = 'green';
