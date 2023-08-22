83fc8190033e6b4a64582b030f7ff4ca57b4175b
Vinay <vinay8309963@gmail.com>
2023-08-22 17:47:58 +0530

    Made list elements into bold and 3rd element into green color

--- dom.js
+++ dom.js
@@ -1,6 +1,10 @@
 var headerTitle = document.getElementById('header-title');
 var header = document.getElementById('main-header');
 header.style.border='solid 6px #000';
-var items = document.getElementsByClassName('title');
-items[0].style.fontWeight = 'bold';
-items[0].style.color = 'green';
+var items = document.getElementsByClassName('list-group-item');
+for(var i = 0;i<items.length;i++)
+{
+    items[i].style.fontWeight = 'bold';
+}
+
+items[2].style.color = 'green';
