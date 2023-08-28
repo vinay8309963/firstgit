e4f091e4e9ac17c05ec72f7433bc82745760ed17
Vinay <vinay8309963@gmail.com>
2023-08-28 23:32:54 +0530

    Expense Tracker App

--- new1.js
+++ new1.js
@@ -1,35 +1,56 @@
-const myForm = document.querySelector('#addForm');
-const expenseAmountInput = document.querySelector('#expenseAmount');
-const descriptionInput = document.querySelector('#description');
-const categoryInput = document.querySelector('#category');
-myForm.addEventListener('submit', onSubmit);
+const myform=document.getElementById('my-form');
+const expense_amount=document.getElementById('expense_amount');
+const description=document.getElementById('description');
+const category=document.getElementById('category');
+const message=document.querySelector('.message');
+const userList=document.getElementById('users');
 
-
-
-function onSubmit(e)
-{
+myform.addEventListener('submit',onSubmit);
+function onSubmit(e){
     e.preventDefault();
+    if(expense_amount.value==='' || description.value===''){
+        message.classList.add('error');
+        message.innerHTML='Please enter all fields';
+        message.style.color='red';
+        setTimeout(()=>message.remove(),3000);
+    }
+    else{
+        const li=document.createElement('li');
+        li.appendChild(document.createTextNode(`${expense_amount.value}-${category.value}-${description.value}`));
+        userList.appendChild(li);
+        //adding delete button
+        var deleteBtn=document.createElement('button');
+        deleteBtn.className='btn btn-sm btn-danger float-right delete';
 
-    if(expenseAmountInput.value === ''|| descriptionInput.value === '' || categoryInput.value === '')
-     {
-         msg.classList.add('error');
-         msg.innerHTML = 'Please enter all fields';
+        //adding edit button
+        var editBtn=document.createElement('button');
+        editBtn.class='btn btn-sm btn-warning float-right delete';
 
-         setTimeout(()=>msg.remove(),3000);
-     }
-     else{
-        
-        let myObj = {
-            Expense : expenseAmountInput.value,
-            Description : descriptionInput.value,
-            Category : categoryInput.value
+        deleteBtn.appendChild(document.createTextNode('Delete Expense'));
+        editBtn.appendChild(document.createTextNode('Edit Expense'));
+
+        deleteBtn.onclick=()=>{
+            expense_amount.value=``;
+            description.value=``;
+            localStorage.removeItem(myobj.expense_amount);
+            userList.removeChild(li);
+        }
+        editBtn.onclick=()=>{
+            expense_amount.value=`${myobj.expense_amount}`;
+            description.value=`${myobj.description}`;
+            localStorage.removeItem(myobj.expense_amount);
+            userList.removeChild(li);
+        }
+        const myobj={
+            expense_amount:expense_amount.value,
+            description:description.value,
+            category:category.value
         };
-    
-     
-        let myObj_serialized = JSON.stringify(myObj);
-        localStorage.setItem(descriptionInput.value, myObj_serialized);
-        expenseAmountInput.value = '';
-        descriptionInput.value = '';
-        categoryInput.value = '';
-     }
+        li.appendChild(deleteBtn);
+        li.appendChild(editBtn);
+        
+
+        //adding to local storage
+        localStorage.setItem(JSON.parse(JSON.stringify(myobj.expense_amount)),JSON.stringify(myobj))
+    }
 }
